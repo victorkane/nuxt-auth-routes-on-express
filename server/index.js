053c21b7166a5e44091b6558f1728a8b5f968da2
victorkane <victorkane@gmail.com>
2018-02-12 17:02:06 -0500

    Initial commit with auth-routes type logic on server side

---
+++ server/index.js
@@ -0,0 +1,80 @@
+
+const express = require('express')
+const session = require('express-session')
+const { Nuxt, Builder } = require('nuxt')
+const app = express()
+const host = process.env.HOST || '127.0.0.1'
+const port = process.env.PORT || 3000
+
+// Transform req & res to have the same API as express
+// So we can use res.status() & res.json()
+/*
+router.use((req, res, next) => {
+  Object.setPrototypeOf(req, app.request)
+  Object.setPrototypeOf(res, app.response)
+  req.res = res
+  res.req = req
+  next()
+})
+*/
+
+app.set('port', port)
+
+// Import and Set Nuxt.js options
+let config = require('../nuxt.config.js')
+config.dev = !(process.env.NODE_ENV === 'production')
+
+// All the server-side stuff before rendering Nuxt!
+
+const bodyParser = require('body-parser')
+
+app.use(bodyParser.urlencoded({
+  extended : true
+}));
+app.use(bodyParser.json())
+
+app.use(session({
+  secret: 'keyboard cat',
+  resave: false,
+  saveUninitialized: false,
+  cookie: { maxAge: 60000 }
+}))
+
+app.get('/about', (req, res) => {
+  res.send('¡Hola, amigos!')
+})
+
+// Add POST - /api/login
+app.post('/api/login', (req, res) => {
+console.log('the body: ', req.body)
+  if (req.body.username === 'demo' && req.body.password === 'demo') {
+    req.session.authUser = { username: 'demo' }
+    return res.json({ username: 'demo' })
+  }
+  res.status(401).json({ message: 'Bad credentials' })
+})
+
+// Add POST - /api/logout
+app.post('/api/logout', (req, res) => {
+  delete req.session.authUser
+  res.json({ ok: true })
+})
+
+async function start() {
+  // Init Nuxt.js
+  const nuxt = new Nuxt(config)
+
+  // Build only in dev mode
+  if (config.dev) {
+    const builder = new Builder(nuxt)
+    await builder.build()
+  }
+
+  // Give nuxt middleware to express
+  app.use(nuxt.render)
+
+  // Listen the server
+  app.listen(port, host)
+  console.log('Server listening on http://' + host + ':' + port) // eslint-disable-line no-console
+}
+start()
